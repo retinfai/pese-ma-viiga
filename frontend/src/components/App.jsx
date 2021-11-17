579a17bdd99f129959bd74b4032a0de65b721e60
retinfai <aferetipama@gmail.com>
2021-11-17 19:59:40 +1300

    Implement Basic Search Screen

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,7 +2,21 @@ import React from "react";
 
 function App(){
     return(
-        <h1>Hello World!</h1>
+
+        <div className={ "center" }>
+            <h1>Pese ma Viiga</h1>
+            <div className={ "search-bar"}>
+                <div>
+                <input type="text" placeholder="Find a Pese" spellCheck="false"/>
+                </div>
+                <div className={"btn-container"}>
+                    <button className="button">Search</button>
+                </div>
+                
+            </div>
+            
+        </div>
+        
     )
 
 }
