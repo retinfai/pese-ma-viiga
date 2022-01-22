38ae177a39083a07c9601b1a40fd39bd6d4f60af
retinfai <aferetipama@gmail.com>
2022-01-22 18:22:01 +1300

    feat: dynamic pese routing

--- components/PeseCard.js
+++ components/PeseCard.js
@@ -1,12 +1,16 @@
 import styles from "../styles/PeseCard.module.css"
+import Link from "next/link"
 
 export default function PeseCard({ peseNum, pese }) {
 
     return (
-        <div className={styles.peseCard}>
-            <h3>Pese {peseNum}</h3>
-            <hr></hr>
-            <p>{pese}</p>
-        </div>
+        <Link href={"pese-" + peseNum}>
+            <div className={styles.peseCard}>
+                <h3>Pese {peseNum}</h3>
+                <hr></hr>
+                <p className={styles.pesePreview}>{pese}</p>
+            </div>
+        </Link>
+
     )
 }
