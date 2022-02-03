79d96bb1b164420a5bcdffafc470f07f60a384fc
retinfai <aferetipama@gmail.com>
2022-02-04 10:28:26 +1300

    feat: fullscreen implemented

--- pages/efks/[pese].js
+++ pages/efks/[pese].js
@@ -4,6 +4,7 @@ import fs from 'fs'
 import styles from '../../styles/PesePage.module.css'
 import Link from 'next/link'
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import { useEffect } from 'react'
 
 export default function PesePage(props) {
 
@@ -11,8 +12,47 @@ export default function PesePage(props) {
     const pese = props.data.pese
     const firstLine = pese.substring(0, pese.indexOf("*"))
 
-    const unformattedPese = pese.split("&")
+    const verses = pese.split("&")
+    const numVerses = verses.length
+    var numLines = 0
+    var numChars = 0
+    verses.forEach(verse => {
+        numLines += verse.split("*").length
+        verse.split("*").forEach(line => numChars += line.length)
+    })
+    console.log(numLines)
+    console.log(numChars)
+
     const handle = useFullScreenHandle();
+
+    useEffect(() => {
+
+        document.addEventListener("fullscreenchange", (event) => {
+            const peseDisplay = document.getElementById(styles.peseDisplay)
+
+            if (document.fullscreenElement || document.webkitCurrentFullScreenElement) {
+                peseDisplay.style.display = "grid"
+                peseDisplay.style.gridTemplateColumns = "repeat(" + Math.ceil(numVerses / 2).toString() + ",1fr"
+                peseDisplay.style.height = "100vh"
+                peseDisplay.style.width = "100vw"
+            } else {
+                peseDisplay.style.display = null;
+                peseDisplay.style.gridTemplateColumns = null
+                peseDisplay.style.height = null
+                peseDisplay.style.width = null
+
+                const lines = Array.prototype.slice.call(document.getElementsByClassName(styles.verseLines))
+
+                lines.forEach(line => {
+                    line.style.marginBottom = null
+                })
+
+            }
+
+        })
+
+
+    }, [])
 
     return (
 
@@ -26,16 +66,16 @@ export default function PesePage(props) {
 
             <FullScreen handle={handle}>
 
-                <div className={styles.peseDisplay}>
+                <div id={styles.peseDisplay}>
                     {
-                        unformattedPese.map((verse, key) => {
+                        verses.map((verse, key) => {
 
                             const lines = verse.split("*")
 
                             return (
                                 <div key={key}>
-
-                                    {lines.map((line, index) => <p key={index}>{line}</p>)}
+                                    <p id={styles.verseNum}>Verse {key + 1}</p>
+                                    {lines.map((line, index) => <p key={index} className={styles.verseLines}>{line}</p>)}
                                     <br />
 
                                 </div>
@@ -47,7 +87,7 @@ export default function PesePage(props) {
                 </div>
 
             </FullScreen>
-            
+
 
         </div>
 
